a862cbf180d9a966c1c6123979e5e340198a87a3
maordr <shangpiu+rdr@gmail.com>
2025-04-17 14:11:05 +0800

    Update mail.php

--- public/mail.php
+++ public/mail.php
@@ -1,10 +1,11 @@
 <?php
-$to      = 'shangpiu+mail@gmail.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+//$to      = 'shangpiu+mail@gmail.com';
+//$subject = 'the subject';
+//$message = 'hello';
+//$headers = 'From: webmaster@example.com' . "\r\n" .
+//    'Reply-To: webmaster@example.com' . "\r\n" .
+//    'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
+//mail($to, $subject, $message, $headers);
+echo("hello");
 ?>
